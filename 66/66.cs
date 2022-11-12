bfbcc1271586889dd9ee5cc6d70aefac17328bc5
Kit <simkatesi@yandex.ru>
2022-11-12 22:07:43 +0400

    вывод чисел в заданном промежутке и нахождение их суммы

---
+++ 66/66.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int GetSumPrint(int start, int end)
+{
+    int sum = 0;
+    if (start>end)
+    {
+        return sum;
+    }
+    Console.WriteLine($"{start}"); // визуальный вывод чисел в промежутке
+    sum = start + GetSumPrint (start + 1, end);  
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число начала отсчета: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число конца отсчета: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма элементов в промежутке от {m} до {n}: {GetSumPrint(m,n)}"); 
