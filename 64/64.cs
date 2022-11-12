95ce1c9f9a2bbb6e98820f1686ff334c06703d8b
Kit <simkatesi@yandex.ru>
2022-11-12 18:16:01 +0400

    рекурсия, выведение чисел в заданном промежутке

---
+++ 64/64.cs
@@ -0,0 +1,18 @@
+﻿//  Задайте значение N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+string GetPrint(int start, int end)
+{
+    if (start==end)
+    {
+        return start.ToString();
+    }
+    return start + " " + GetPrint (start - 1, end);
+}
+
+ Console.Clear();
+ Console.Write("Введите число: ");
+ int n = int.Parse(Console.ReadLine()!);
+ int m = 1;
+ Console.WriteLine(GetPrint(n,m));
